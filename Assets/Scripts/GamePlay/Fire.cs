9f0c7fc6554b9f52f985fe2f7c7dfcdc81b6e179
Matheus <matheus.mello@vetorzero.com.br>
2021-02-04 18:32:22 -0300

    player atirando ruim

---
+++ Assets/Scripts/GamePlay/Fire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+using Physics = DefaultNamespace.Physics;
+
+public class Fire : MonoBehaviour
+{
+
+    private Physics _physics;
+
+    private Settings _settings;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _physics = new Physics();
+        _settings = Bootstrap.Instance.Settings;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += -_settings.ScrollDirection * (Time.deltaTime * _settings.ScrollSpeed*2);
+    }
+}
