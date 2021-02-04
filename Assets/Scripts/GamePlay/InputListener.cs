1317fdaa757f1f6ec13eeeb7926d4fbe10f2d8e6
Yuri Cruz <yuriaicruz@gmail.com>
2021-02-04 13:58:47 -0300

    pause and navigate

--- Assets/Scripts/GamePlay/InputListener.cs
+++ Assets/Scripts/GamePlay/InputListener.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class InputListener : MonoBehaviour
 {
-    public static event Action Jump;
-    public static event Action GravityChange; 
-    public static event Action Attack;
-    public static event Action<Vector2Int> Navigate;
+    public event Action Jump;
+    public event Action GravityChange; 
+    public event Action Attack;
+    public event Action<Vector2Int> Navigate;
+    public event Action Pause;
 
     private void Update()
     {
@@ -27,21 +28,26 @@ public class InputListener : MonoBehaviour
             if (Attack != null) Attack();
         }
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause?.Invoke();
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            
+            Navigate?.Invoke(Vector2Int.left);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            
+            Navigate?.Invoke(Vector2Int.right);
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            
+            Navigate?.Invoke(Vector2Int.up);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            
+            Navigate?.Invoke(Vector2Int.down);
         }
     }
 }
